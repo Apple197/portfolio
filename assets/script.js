b34734ebae61155a7144d336572c04156462e3e2
Nazrul Hasan <appleios986@gmail.com>
2026-01-19 00:34:23 +0530

    I have completed the site code

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+const menuButton = document.querySelector(".hamburger-menu");
+const body = document.querySelector("body");
+const isNavOpen = document.querySelector("#header nav");
+const themeToggleButtom = document.querySelector(".theme-mode");
+let isLightThemeActive = localStorage.getItem("theme-mode");
+
+// Navigation Menu Logic's Start Here
+menuButton.addEventListener("click", () => {
+  body.classList.toggle("active-menu");
+});
+
+isNavOpen.addEventListener("click", () => {
+  if (body.classList.contains("active-menu")) {
+    body.classList.toggle("active-menu");
+  }
+});
+
+// Theme Mode Logic's Start Here
+const lightMode = () => {
+  body.classList.add("light-theme");
+  localStorage.setItem("theme-mode", "Light");
+};
+
+const darkMode = () => {
+  body.classList.remove("light-theme");
+  localStorage.setItem("theme-mode", "Dark");
+};
+
+if (isLightThemeActive === "Light") {
+  lightMode();
+}
+
+themeToggleButtom.addEventListener("click", () => {
+  isLightThemeActive = localStorage.getItem("theme-mode");
+  isLightThemeActive !== "Light" ? lightMode() : darkMode();
+});
+
+// Lenis smooth scroll Logic's Start Here
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  //console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
